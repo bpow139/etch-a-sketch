ee7c98902fdb0635dfcd631b978b8d67cf9cd77b
Brett Powers <brett.m.powers@gmail.com>
2022-11-18 02:43:42 -0700

    can change grid size.

--- javascript.js
+++ javascript.js
@@ -1,38 +1,35 @@
-const GRID_SIZE = 16 * 16;
+const container = document.querySelector('.container');
 
-//maybe for loop to create each div from the given grid size.
+function createGrid(gridSize) {
 
-let container = document.querySelector('.container');
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
 
-for (let i = 0; i < GRID_SIZE; i++) {
-    let div = document.createElement('div');
-    div.classList.add('grid-item');
-    container.appendChild(div);
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-item');
+        container.appendChild(div);
+    }
+
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener("mouseover", (e) => {
+            e.target.classList.add('hover');
+        });
+    })
 }
 
+//initial grid size of 16
+createGrid(16);
 
-let gridItems = document.querySelectorAll('.grid-item');
+let btn = document.querySelector('button');
+btn.addEventListener('click', gridSizeChange);
 
-gridItems.forEach(gridItem => {
-    gridItem.addEventListener("mouseover", (e) => {
-        e.target.classList.add('hover');
-    });
-})
-
-gridItems.forEach(gridItem => {
-    gridItem.addEventListener("mouseout", (e) => {
-        e.target.classList.remove('hover');
-    });
-})
-
-
-function hoverOn() {
-
-    //document.querySelector('grid-item').classList.add('hover');
-}
-
-function hoverOff() {
-
-    //document.querySelector('grid-item').classList.remove('hover');
+function gridSizeChange() {
+    let gridSize = prompt('What grid size?');
+    createGrid(gridSize);
 }
