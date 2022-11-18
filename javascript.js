5e117a86db068167b69efcf70e188d273b9833ab
Brett Powers <brett.m.powers@gmail.com>
2022-11-18 01:36:16 -0700

    add in hover coloring effect

--- javascript.js
+++ javascript.js
@@ -1,14 +1,38 @@
-const GRID_SIZE = 16;
+const GRID_SIZE = 16 * 16;
 
 //maybe for loop to create each div from the given grid size.
 
 let container = document.querySelector('.container');
 
-let div = document.createElement('div');
-container.appendChild(div);
-
 
 for (let i = 0; i < GRID_SIZE; i++) {
     let div = document.createElement('div');
+    div.classList.add('grid-item');
     container.appendChild(div);
+}
+
+
+let gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach(gridItem => {
+    gridItem.addEventListener("mouseover", (e) => {
+        e.target.classList.add('hover');
+    });
+})
+
+gridItems.forEach(gridItem => {
+    gridItem.addEventListener("mouseout", (e) => {
+        e.target.classList.remove('hover');
+    });
+})
+
+
+function hoverOn() {
+
+    //document.querySelector('grid-item').classList.add('hover');
+}
+
+function hoverOff() {
+
+    //document.querySelector('grid-item').classList.remove('hover');
 }
